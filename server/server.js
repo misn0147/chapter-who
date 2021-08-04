187ce96a9d7eb76860aaa6215429614100e380c1
M Natoli <natoli.missy@gmail.com>
2021-08-03 21:36:18 -0500

    added typedefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
